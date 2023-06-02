8a9460a83681e8f6b6240d4f5788da04a0cd695f
Lerigou <marciaeduardaravadelli@gmail.com>
2023-06-01 22:18:36 -0300

    Acabei, eba

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var email = document.getElementById("idEmail");
+var feedback = document.getElementById("idFeedback");
+
+function enviar() {
+
+    if (email != "" & feedback != "") {
+        alert('Obrigado por deixar a sua opinião!');
+    }
+
+}
